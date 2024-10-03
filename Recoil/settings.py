9606687781a0a82823851c2c6ebd66e44f20c256
Vort <enochtemitope16@gmail.com>
2024-10-03 15:18:58 +0100

    Made some changes to the admin

--- Recoil/settings.py
+++ Recoil/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Modified this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
